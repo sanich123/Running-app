80e53535d7a499c86f8737aeb6728c6b30c9df13
Alexander Voronin <aovoronin.piano@gmail.com>
2023-06-02 19:44:40 +0300

    init: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+To watch our wonderful android app, you should go by these steps:
+
+1. If you dont't have eas-cli = npm install -g eas-cli
+
+2. Install the dependencies = {npm, yarn} install 
+
+//npx expo install expo-dev-client expo-location must be
+
+Build an apk file = {npm run, yarn} buildApk //eas build --profile development --platform android
+
+3. When it will be necessary, type the following in the command line:
+
+'user' : sanich123 
+
+'password' : 7fwd7rlm
+
+With QR-code or the direct link download the app.
+
+4. To run development = {npm, yarn} run startDev //npx expo start --dev-client
+
+5. Scan the QR-code in ExpoGo
+
+6. Fucking enjoy your app into the mobile
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,63 @@
-import { StyleSheet } from 'react-native';
+import "expo-dev-client";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Mapbox, { PointAnnotation, Callout, MapView, Camera } from "@rnmapbox/maps";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+Mapbox.setWellKnownTileServer("Mapbox");
+Mapbox.setAccessToken("pk.eyJ1Ijoic2FuaWNoMTIzIiwiYSI6ImNsaWFkNmptaDAyaTczcm11NHF0cmp3d2sifQ.ZKH9THateIfnZ7zC23f3-g");
 
-export default function TabOneScreen() {
+export default function App() {
+  const [calloutVisible, setCalloutVisible] = useState(false);
+  const [coordinates] = useState([-5, 55]);
+  const onMarkerPress = () => {
+    setCalloutVisible(true);
+  };
+
+  const loadAnnotationUK = () => {
+    return (
+      <PointAnnotation key="annotationUK" id="annotationUK" coordinate={[0.1, 51.5]} onSelected={onMarkerPress}>
+        <View
+          style={{
+            height: 20,
+            width: 20,
+            backgroundColor: "green",
+            borderColor: "black",
+            borderWidth: 2,
+            borderRadius: 50,
+          }}
+        ></View>
+
+        <Callout title="Welcome to London!" contentStyle={{ borderRadius: 5 }}></Callout>
+      </PointAnnotation>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapView style={styles.map}>
+          <Camera zoomLevel={4} centerCoordinate={coordinates} />
+          <PointAnnotation id="uk" coordinate={coordinates}><View/></PointAnnotation>
+          <View>{loadAnnotationUK()}</View>
+        </MapView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+
+  container: {
+    height: "100%",
+    width: "100%",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+ 
+  map: {
+    flex: 1,
   },
 });
