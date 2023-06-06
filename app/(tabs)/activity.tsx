e507487dcc4578d8028c99ae88f968b39ff4f378
Alexander Voronin <aovoronin.piano@gmail.com>
2023-06-06 23:19:28 +0300

    feat: work owith maps

--- app/(tabs)/activity.tsx
+++ app/(tabs)/activity.tsx
@@ -4,11 +4,8 @@ import { Text, View } from "../../components/Themed";
 import "expo-dev-client";
 import React, { useState } from "react";
 
-import Mapbox, { PointAnnotation, Callout, MapView, Camera } from "@rnmapbox/maps";
+import Mapbox, { PointAnnotation, Callout, MapView, Camera, UserLocation, MarkerView } from "@rnmapbox/maps";
 import { useAppSelector } from "../../redux/hooks/hooks";
-
-Mapbox.setWellKnownTileServer("Mapbox");
-Mapbox.setAccessToken("pk.eyJ1Ijoic2FuaWNoMTIzIiwiYSI6ImNsaWFkNmptaDAyaTczcm11NHF0cmp3d2sifQ.ZKH9THateIfnZ7zC23f3-g");
 
 export default function Activity() {
   const {
@@ -21,9 +18,13 @@ export default function Activity() {
     <View style={styles.page}>
       <View style={styles.container}>
         <MapView style={styles.map}>
-          <Camera zoomLevel={15} centerCoordinate={[longitude, latitude]} animationMode="flyTo" animationDuration={2000} />
-          <PointAnnotation id="your-home" coordinate={[longitude, latitude]}>
-            <Text>Твой дом, мазафака, находится где-то тут. Я тебя по IP вычислил</Text>
+          <Camera zoomLevel={15} centerCoordinate={[longitude, latitude]} animationMode="flyTo" animationDuration={2000} followUserLocation />
+          <MarkerView coordinate={[longitude, latitude]} />
+          <PointAnnotation coordinate={[longitude, latitude]} id="home">
+            <View
+              style={styles.customHome}
+            ></View>
+            <Callout title="Че тыкаешь?"/>
           </PointAnnotation>
         </MapView>
       </View>
@@ -45,5 +46,13 @@ const styles = StyleSheet.create({
 
   map: {
     flex: 1,
+  },
+  customHome: {
+    height: 20,
+    width: 20,
+    backgroundColor: "green",
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 50,
   },
 });
--- hooks/use-get-location.ts
+++ hooks/use-get-location.ts
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import { LocationObject, requestForegroundPermissionsAsync, getCurrentPositionAsync } from "expo-location";
 import { useAppDispatch } from "../redux/hooks/hooks";
 import { setInitialLocation } from "../redux/location-slice/location-slice";
+import Mapbox from "@rnmapbox/maps";
 
 export default function useGetLocation() {
+  Mapbox.setWellKnownTileServer("Mapbox");
+  Mapbox.setAccessToken("pk.eyJ1Ijoic2FuaWNoMTIzIiwiYSI6ImNsaWFkNmptaDAyaTczcm11NHF0cmp3d2sifQ.ZKH9THateIfnZ7zC23f3-g");
+
   const [location, setLocation] = useState<LocationObject>();
   const [error, setErrorMsg] = useState(false);
   const dispatch = useAppDispatch();
+
   useEffect(() => {
     (async () => {
       let { status } = await requestForegroundPermissionsAsync();
@@ -19,5 +24,6 @@ export default function useGetLocation() {
       dispatch(setInitialLocation(location))
     })();
   }, []);
+  
   return { location, error };
 }
