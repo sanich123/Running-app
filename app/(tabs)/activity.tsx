a4d0004508012685872a5d2db1f9fa241b3b09a5
Alexander Voronin <aovoronin.piano@gmail.com>
2023-06-09 13:10:50 +0300

    feat: hurray, finally done with fake locations

--- app/(tabs)/activity.tsx
+++ app/(tabs)/activity.tsx
@@ -2,9 +2,8 @@ import { StyleSheet } from "react-native";
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import "expo-dev-client";
-import React, { useState } from "react";
 
-import Mapbox, { PointAnnotation, Callout, MapView, Camera, UserLocation, MarkerView, ShapeSource, LineLayer } from "@rnmapbox/maps";
+import { PointAnnotation, Callout, MapView, Camera, UserLocation, ShapeSource, LineLayer } from "@rnmapbox/maps";
 import { useAppSelector } from "../../redux/hooks/hooks";
 import useFakeLocations from "../../hooks/use-fake-locations";
 
@@ -14,20 +13,22 @@ export default function Activity() {
       coords: { latitude, longitude },
     },
   } = useAppSelector(({ location }) => location);
-  const locations = useFakeLocations();
+  const { locations, cameraRef } = useFakeLocations();
 
   return (
     <View style={styles.page}>
       <View style={styles.container}>
         <MapView style={styles.map}>
           <UserLocation androidRenderMode="compass" animated />
-          <Camera centerCoordinate={[longitude, latitude]} animationMode="flyTo" animationDuration={2000} followUserLocation />
-          <PointAnnotation coordinate={[longitude, latitude]} id="home">
-            <View style={styles.customHome}></View>
-            <Callout title="Че тыкаешь?" />
-          </PointAnnotation>
+          <Camera ref={cameraRef} centerCoordinate={[latitude, longitude]} animationMode="flyTo" animationDuration={2000} zoomLevel={12} />
+          {locations.map((location) => (
+            <PointAnnotation key={Math.random().toString()} coordinate={location} id="home">
+              <View style={styles.customHome}></View>
+              <Callout title={`Координаты ${location[0]}, ${location[1]}`} />
+            </PointAnnotation>
+          ))}
           <ShapeSource
-          id="shape-source"
+            id="shape-source"
             shape={{
               type: "FeatureCollection",
               features: [
@@ -36,10 +37,7 @@ export default function Activity() {
                   properties: {},
                   geometry: {
                     type: "LineString",
-                    coordinates: [
-                      [31.59757761529764, 46.63755091385189],
-                      [27.711524020005644, 46.19689011566155],
-                    ],
+                    coordinates: locations,
                   },
                 },
               ],
--- hooks/use-fake-locations.ts
+++ hooks/use-fake-locations.ts
@@ -1,16 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MOCK_LOCATIONS } from "../constants/mocks/mocks";
 
+import { Camera } from "@rnmapbox/maps";
 export default function useFakeLocations() {
   const [duration, setDuration] = useState(0);
   const [locations, setLocations] = useState([MOCK_LOCATIONS[0]]);
-
+  const camera = useRef<Camera>(null);
   useEffect(() => {
     const interval = setInterval(() => {
       setDuration((duration) => duration + 1);
-      setLocations((locations) => [...locations, MOCK_LOCATIONS[duration]]);
     }, 1000);
     return () => clearInterval(interval);
   }, []);
-  return locations;
+
+  useEffect(() => {
+    if (duration < MOCK_LOCATIONS.length) {
+      setLocations([...locations, MOCK_LOCATIONS[duration]]);
+    }
+  }, [duration]);
+
+  useEffect(() => {
+    camera.current?.setCamera({
+      centerCoordinate: locations[locations.length - 1],
+    });
+  }, [locations]);
+
+  return {
+    locations,
+    lastView: locations[locations.length - 1],
+    cameraRef: camera
+  };
 }
