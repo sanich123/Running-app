92ed67eef7727471b7c65b83afbf30cd610e909e
Alexander <aovoronin.piano@gmail.com>
2023-08-22 13:40:30 +0300

    merging

--- app/(tabs)/progress.tsx
+++ app/(tabs)/progress.tsx
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { View, Text } from '../../components/Themed';
 
-
 export default function Progress() {
   return (
     <View style={styles.container}>
