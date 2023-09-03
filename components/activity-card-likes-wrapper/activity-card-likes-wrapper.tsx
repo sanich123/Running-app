2d26c0e9ec6c91fbdc9a8ae9597bdfed9182427b
Alexander <aovoronin.piano@gmail.com>
2023-09-03 12:41:50 +0300

    Rn paper/styling (#16)
    * feat: add react-native-paper and asyncStorage
    
    * feat: add asyncStorage, a couple of tests
    
    * feat: a couple of tests
    
    * feat: working on routing
    
    * fix: styling
    
    * feat: work on routing, authentication
    
    * feat: working on authentication
    
    * feat: add basic authentication, everything is working
    
    * merging
    
    * feat: working on validating inputs
    
    * feat: implement firebase authentication with email and password
    
    * feat: implement image picker
    
    * fix: small refactoring
    
    * feat: experiment with date-picker
    
    * Revert "feat: experiment with date-picker"
    
    This reverts commit a5c5f74deec3d31a6bc7853a7b1cce4cd0af0de1.
    
    * fix: test
    
    * feat: rewrite sign-in component with useContext logic
    
    * feat: work on errors in inputs
    
    * feat: work on routing
    
    * feat: working with the map, migrate from mocks to random location
    
    * fix: small fix
    
    * fix: fix small things
    
    * feat: basic rtk, working on scrollView and flatlist
    
    * fix: another fix
    
    * feat: implement storing images on firebase cloud
    
    * feat: working on progress indicator, when uploading a file
    
    * feat: work on profile-settings page
    
    * fix: fix eslint
    
    * feat: saving before experiment with datepicker
    
    * feat: add datepicker, rebuilding
    
    * feat: implement settings page
    
    * feat: work on disabled, loading state of components
    
    * feat: working on settings page
    
    * feat: small fix
    
    * feat: add redux-persistor
    
    * feat: basically implement register and login functionality
    
    * feat: add saving profile logic
    
    * feat: work on profile editing
    
    * feat: work on saving activity and profile info
    
    * feat: fix bug with sending activity to the server
    
    * feat: implement connection between saving activity and showing activities in a main page
    
    * feat: working on feed list
    
    * feat: add a couple of methods to runich-api, and refactoring
    
    * feat: working on comments
    
    * feat: basically implement comments and likes
    
    * fix: fix small bug
    
    * fix: small

--- components/activity-card-likes-wrapper/activity-card-likes-wrapper.tsx
+++ components/activity-card-likes-wrapper/activity-card-likes-wrapper.tsx
@@ -10,7 +10,7 @@ export default function ActivityCardLikesWrapper({ activityId }: { activityId: s
     <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
       {isLoading && <ActivityIndicator />}
       {error ? <Text variant="bodyMedium">An error occured</Text> : null}
-      {likes ? likes?.map(({ authorId }) => <AvatarShowable size={25} id={authorId} />) : null}
+      {likes ? likes?.map(({ authorId, id }) => <AvatarShowable size={25} id={authorId} key={id} />) : null}
       {likes?.length ? <Text variant="bodyMedium">{`${likes.length} guys send like to your activity`}</Text> : null}
     </View>
   );
