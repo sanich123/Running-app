29ae9c21382ac54f98240a8d7c5234e8f888ff6c
Alexander <aovoronin.piano@gmail.com>
2023-09-06 09:34:05 +0300

    feat: commitn from laptop

--- app/(tabs)/users/index.tsx
+++ app/(tabs)/users/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from 'react';
 import { View, FlatList, SafeAreaView } from 'react-native';
 import { ActivityIndicator, Divider, Text } from 'react-native-paper';
 
@@ -6,11 +7,19 @@ import UserListItem from '../../../components/user-list-item/user-list-item';
 import { useGetUsersQuery } from '../../../redux/runnich-api/runnich-api';
 
 export default function ListOfUsers() {
-  const { isLoading, error, data: users } = useGetUsersQuery('');
+  const { isLoading, error, data: users, refetch } = useGetUsersQuery('');
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch();
+    setTimeout(() => setRefreshing(false), 2000);
+  }, []);
   return (
     <SafeAreaView style={[{ flex: 1 }, isLoading && { alignItems: 'center', justifyContent: 'center' }]}>
       {users && (
         <FlatList
+          onRefresh={onRefresh}
+          refreshing={refreshing}
           data={users}
           renderItem={({ item }) => <UserListItem userId={item.id} />}
           ListEmptyComponent={
