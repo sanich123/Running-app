8e89bf50080720635af2fa35825051386bd50006
Alexander <aovoronin.piano@gmail.com>
2023-09-16 18:40:11 +0300

    feat: anotherexperiment with preview

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,24 +5,14 @@ import { initializeAuth, getReactNativePersistence, GoogleAuthProvider } from 'f
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-const {
-  EXPO_PUBLIC_FIREBASE_APIKEY,
-  EXPO_PUBLIC_AUTH_DOMAIN,
-  EXPO_PUBLIC_PROJECT_ID,
-  EXPO_PUBLIC_STORAGE_BUCKET,
-  EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  EXPO_PUBLIC_APP_ID,
-  EXPO_PUBLIC_MEASUREMENTID,
-} = process.env;
-
 const firebaseConfig = {
-  apiKey: EXPO_PUBLIC_FIREBASE_APIKEY,
-  authDomain: EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: EXPO_PUBLIC_APP_ID,
-  measurementId: EXPO_PUBLIC_MEASUREMENTID,
+  apiKey: 'AIzaSyDnyP44gzUApSZKnLZihdVMHhzt_gL0GF0',
+  authDomain: 'runich-6be07.firebaseapp.com',
+  projectId: 'runich-6be07',
+  storageBucket: 'runich-6be07.appspot.com',
+  messagingSenderId: 'EXPO_PUBLIC_MESSAGING_SENDER_ID',
+  appId: '1:335123904905:web:22dd72162fb091b9920958',
+  measurementId: 'G-3TE0WYH8JX',
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
