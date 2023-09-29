80041f9535a9b3eefa3935973f70679146320db4
Alexander <aovoronin.piano@gmail.com>
2023-09-29 14:22:26 +0300

    fix: fix a small bug

--- redux/activity/activity.ts
+++ redux/activity/activity.ts
@@ -13,7 +13,7 @@ export const activity = createSlice({
       description: '',
       sport: '',
       emotion: '',
-      isSwitchOn: '',
+      isSwitchOn: false,
       photoUrls: [] as string[],
     },
   },
