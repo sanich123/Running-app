9c99b0c5c7e70f10eb60eece4fc4fcebd6525cc2
Alexander <aovoronin.piano@gmail.com>
2023-09-30 13:18:49 +0300

    feat: working on resending manually activity, when error occured

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,8 +1,6 @@
 import { useRouter } from 'expo-router';
-import { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList, ToastAndroid } from 'react-native';
+import { SafeAreaView, FlatList } from 'react-native';
 import { ActivityIndicator, Divider } from 'react-native-paper';
-import { useDispatch, useSelector } from 'react-redux';
 
 import { useAuth } from '../../../auth/context/auth-context';
 import ActivitySendingIndicator from '../../../components/activity-sending-indicator/activity-sendinig-indicator';
@@ -10,60 +8,17 @@ import ActivityCard from '../../../components/card/card';
 import EmptyActivitiesList from '../../../components/empty-activities-list/empty-activities-list';
 import ErrorComponent from '../../../components/error-component/error-component';
 import FloatingBtn from '../../../components/floating-btn/floating-btn';
-import { setIsNeedToSendActivity } from '../../../redux/activity/activity';
-import {
-  useAddActivityByUserIdMutation,
-  useGetActivitiesByUserIdWithFriendsActivitiesQuery,
-} from '../../../redux/runich-api/runich-api';
-import { errorHandler } from '../../../utils/error-handler';
+import { useGetActivitiesByUserIdWithFriendsActivitiesQuery } from '../../../redux/runich-api/runich-api';
 import useGetLocation from '../../../utils/hooks/use-get-location';
 import useRefresh from '../../../utils/hooks/use-refresh';
 
 export default function Feed() {
   const { user } = useAuth();
-  const dispatch = useDispatch();
+  const router = useRouter();
   useGetLocation();
   const { data: activities, error, isLoading, refetch } = useGetActivitiesByUserIdWithFriendsActivitiesQuery(user.id);
-  const [sendActivity] = useAddActivityByUserIdMutation();
   const { onRefresh, refreshing } = useRefresh(refetch);
-  const router = useRouter();
-  const { activityToSend, isNeedToSend } = useSelector(({ activity }) => activity);
-  const [isErrorSending, setIsErrorSending] = useState(false);
-  const [isStartSending, setIsStartSending] = useState(false);
-  const [isSuccessSending, setIsSuccessSending] = useState(false);
-  console.log(isNeedToSend, isStartSending, isErrorSending, isSuccessSending);
 
-  useEffect(() => {
-    if (isNeedToSend) {
-      setIsStartSending(true);
-      sendActivityToServer();
-      dispatch(setIsNeedToSendActivity(false));
-    }
-  }, [isNeedToSend]);
-
-  async function sendActivityToServer() {
-    try {
-      await sendActivity({ body: activityToSend, id: user.id })
-        .unwrap()
-        .then((data) => {
-          console.log(data);
-          ToastAndroid.show('Successfully sended data to server!', ToastAndroid.SHORT);
-          dispatch(setIsNeedToSendActivity(false));
-          setIsSuccessSending(true);
-        })
-        .catch((error) => {
-          setIsErrorSending(true);
-          ToastAndroid.show('Some error occured', ToastAndroid.SHORT);
-          console.log(error);
-        })
-        .finally(() => {
-          setIsStartSending(false);
-          setTimeout(() => setIsSuccessSending(false), 1000);
-        });
-    } catch (error) {
-      errorHandler(error);
-    }
-  }
   return (
     <>
       <SafeAreaView
@@ -94,11 +49,7 @@ export default function Feed() {
               );
             }}
             ListEmptyComponent={<EmptyActivitiesList />}
-            ListHeaderComponent={
-              (isStartSending || isErrorSending) && (
-                <ActivitySendingIndicator isStartSending={isStartSending} isErrorSending={isErrorSending} />
-              )
-            }
+            ListHeaderComponent={<ActivitySendingIndicator />}
             initialNumToRender={5}
             ItemSeparatorComponent={() => <Divider />}
           />
--- components/activity-sending-indicator/activity-sendinig-indicator.tsx
+++ components/activity-sending-indicator/activity-sendinig-indicator.tsx
@@ -1,36 +1,98 @@
-import { View, Pressable } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Pressable, ToastAndroid } from 'react-native';
 import { Text } from 'react-native-paper';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { useAuth } from '../../auth/context/auth-context';
 import { setIsNeedToSendActivity } from '../../redux/activity/activity';
+import { useAddActivityByUserIdMutation } from '../../redux/runich-api/runich-api';
+import { errorHandler } from '../../utils/error-handler';
 
-export default function ActivitySendingIndicator({
-  isErrorSending,
-  isStartSending,
-}: {
-  isErrorSending: boolean;
-  isStartSending: boolean;
-}) {
+export default function ActivitySendingIndicator() {
+  const { user } = useAuth();
   const dispatch = useDispatch();
+  const { activityToSend, isNeedToSend } = useSelector(({ activity }) => activity);
+  const [sendActivity] = useAddActivityByUserIdMutation();
+  const [isFetching, setIsFetching] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    if (isNeedToSend) {
+      setIsFetching(true);
+      sendActivityToServer();
+      dispatch(setIsNeedToSendActivity(false));
+    }
+  }, [isNeedToSend]);
+
+  async function sendActivityToServer() {
+    try {
+      await sendActivity({ body: activityToSend, id: user.id })
+        .unwrap()
+        .then(() => {
+          setIsFetching(false);
+          setIsSuccess(true);
+          setIsError(false);
+          setTimeout(() => setIsSuccess(false), 2000);
+        })
+        .catch((error) => {
+          setIsFetching(false);
+          setIsSuccess(false);
+          setIsError(true);
+          ToastAndroid.show('Some error occured', ToastAndroid.SHORT);
+          console.log(error);
+        });
+    } catch (error) {
+      setIsFetching(false);
+      setIsSuccess(false);
+      setIsError(true);
+      ToastAndroid.show('Some error occured', ToastAndroid.SHORT);
+      console.log(error);
+      errorHandler(error);
+    }
+  }
   return (
-    <Pressable
-      onPress={() => {
-        if (isErrorSending) {
-          dispatch(setIsNeedToSendActivity(true));
-        }
-      }}>
-      <View
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: 30,
-          backgroundColor: !isErrorSending ? 'green' : 'red',
-        }}>
-        <Text variant="bodyMedium">
-          {isStartSending ? 'Sending an activity' : 'An error occured, try to refetch manually'}
-        </Text>
-      </View>
-    </Pressable>
+    <>
+      {(isNeedToSend || isFetching || isSuccess || isError) && (
+        <>
+          <View
+            style={[
+              {
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 30,
+              },
+              isFetching && { backgroundColor: 'yellow' },
+              isError && { backgroundColor: 'red' },
+              isSuccess && { backgroundColor: 'green' },
+            ]}>
+            <Text variant="bodyMedium">
+              {isFetching && 'Sending an activity...'}
+              {isSuccess && 'Successfully sended!'}
+            </Text>
+          </View>
+          <Pressable
+            onPress={() => {
+              if (isError) {
+                dispatch(setIsNeedToSendActivity(true));
+              }
+            }}>
+            <View
+              style={[
+                {
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  height: 30,
+                },
+                isError && { backgroundColor: 'red' },
+              ]}>
+              <Text variant="bodyMedium"> {isError && 'An error occured, try manually'}</Text>
+            </View>
+          </Pressable>
+        </>
+      )}
+    </>
   );
 }
