0d858d26a3c3219c5d4f5ad839f67325c70e394f
Alexander <aovoronin.piano@gmail.com>
2023-11-26 17:05:39 +0300

    feat: add scrollView to a photos-grid page

--- app/(tabs)/home/media-grid/[id].tsx
+++ app/(tabs)/home/media-grid/[id].tsx
@@ -1,7 +1,7 @@
 import ErrorComponent from '@C/error-component/error-component';
 import { useGetAllActivityPhotosByUserIdQuery } from '@R/runich-api/runich-api';
 import { useLocalSearchParams, useRouter } from 'expo-router';
-import { Pressable, ScrollView, useWindowDimensions, Image, StyleSheet } from 'react-native';
+import { Pressable, ScrollView, useWindowDimensions, Image, StyleSheet, View } from 'react-native';
 import { ActivityIndicator, useTheme } from 'react-native-paper';
 
 export default function MediaGrid() {
@@ -14,27 +14,29 @@ export default function MediaGrid() {
   const calculatedWidth = (width - gap * 3) / 4;
 
   return (
-    <ScrollView
-      style={[
-        {
-          backgroundColor: theme.colors.background,
-          gap,
-        },
-        styles.layout,
-        (isLoading || isError) && styles.isInCenter,
-      ]}>
-      {isLoading && <ActivityIndicator size="large" />}
-      {isError ? <ErrorComponent error={error} /> : null}
-      {isSuccess &&
-        !isError &&
-        photos
-          ?.map(({ photoUrls }: { photoUrls: string[] }) => photoUrls)
-          .flat()
-          .map((url: string, index: number) => (
-            <Pressable key={`${url}+${index}`} onPress={() => push(`/home/media/${encodeURIComponent(url)}`)}>
-              <Image key={`${url}+${index}`} source={{ uri: url }} height={calculatedWidth} width={calculatedWidth} />
-            </Pressable>
-          ))}
+    <ScrollView>
+      <View
+        style={[
+          {
+            backgroundColor: theme.colors.background,
+            gap,
+          },
+          styles.layout,
+          (isLoading || isError) && styles.isInCenter,
+        ]}>
+        {isLoading && <ActivityIndicator size="large" />}
+        {isError ? <ErrorComponent error={error} /> : null}
+        {isSuccess &&
+          !isError &&
+          photos
+            ?.map(({ photoUrls }: { photoUrls: string[] }) => photoUrls)
+            .flat()
+            .map((url: string, index: number) => (
+              <Pressable key={`${url}+${index}`} onPress={() => push(`/home/media/${encodeURIComponent(url)}`)}>
+                <Image key={`${url}+${index}`} source={{ uri: url }} height={calculatedWidth} width={calculatedWidth} />
+              </Pressable>
+            ))}
+      </View>
     </ScrollView>
   );
 }
