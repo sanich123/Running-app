8de5d37fa4e4e37f7adf2ef67a04af24e2995860
Alexander <aovoronin.piano@gmail.com>
2024-01-08 01:25:02 -0800

    feat: fix ts errors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
-import { AuthProvider } from '@auth/context/auth-context';
-import SplashIcon from '@c/splash-screen/splash-screen';
-import { persistor, store } from '@r/store';
+import { AuthProvider } from '@A/context/auth-context';
+import SplashIcon from '@C/splash-screen/splash-screen';
+import { persistor, store } from '@R/store';
+import { useGetFontsThemeSettings } from '@U/hooks/use-get-fonts-theme-settings';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useGetFontsThemeSettings } from '@u/hooks/use-get-fonts-theme-settings';
 import { Slot } from 'expo-router';
 import { AppRegistry } from 'react-native';
 import { ActivityIndicator, MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper';
@@ -10,10 +10,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import expo from '../app.config';
-import { AuthProvider } from '../auth/context/auth-context';
-import SplashIcon from '../components/splash-screen/splash-screen';
-import { persistor, store } from '../redux/store';
-import { useGetFontsThemeSettings } from '../utils/hooks/use-get-fonts-theme-settings';
 
 export default function RootLayout() {
   const { loaded, colorScheme, theme } = useGetFontsThemeSettings();
