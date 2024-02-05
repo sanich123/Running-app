6040b24c68760bd2d5e15ab1c6583c70b16e5322
Alexander <aovoronin.piano@gmail.com>
2024-02-05 10:00:27 +0300

    feat: tryeing to implement web support

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,13 @@ const { getDefaultConfig } = require('@expo/metro-config');
 // eslint-disable-next-line no-undef
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
+defaultConfig.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === '@rnmapbox/maps') {
+    return {
+      type: 'empty',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = defaultConfig;
