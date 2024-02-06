da2c1a55166eb02066955ea829df027a0ab1542d
Alexander <aovoronin.piano@gmail.com>
2024-02-06 21:46:08 +0300

    feat: dev ios app builds good

--- metro.config.js
+++ metro.config.js
@@ -6,7 +6,8 @@ defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.resolveRequest = (context, moduleName, platform) => {
   if (
     (platform === 'web' && moduleName === '@rnmapbox/maps') ||
-    (platform === 'android' && moduleName === 'expo-image')
+    (platform === 'android' && moduleName === 'expo-image') ||
+    (platform === 'ios' && moduleName === 'expo-image')
   ) {
     return {
       type: 'empty',
