bc09480b172f1a69bcf1e7efd7fa1042ecd35ca1
Alexander <aovoronin.piano@gmail.com>
2024-02-08 19:13:04 +0300

    feat: working on mapbox-gl

--- components/display-activity-map/display-activity-map.tsx
+++ components/display-activity-map/display-activity-map.tsx
@@ -1,8 +1,10 @@
+import MapboxWeb from '@C/mapbox-web/mapbox-web';
 import { LastKmSplit } from '@R/location/types';
 import { MapView, Camera } from '@rnmapbox/maps';
 import bbox from '@turf/bbox';
 import { LocationObject } from 'expo-location';
 import { useRef, useEffect } from 'react';
+import { Platform } from 'react-native';
 //@ts-ignore
 import lineString from 'turf-linestring';
 
@@ -19,7 +21,6 @@ export default function DisplayActivityMap({ locations, kilometresSplit }: Displ
   const line = lineString(modifiedLocationsForTurf);
   const [minLat, minLng, maxLat, maxLng] = bbox(line);
   const cameraRef = useRef<Camera>(null);
-
   useEffect(() => {
     setTimeout(() => {
       cameraRef.current?.fitBounds([minLng, minLat], [maxLng, maxLat], [20, 20], 1000);
@@ -27,16 +28,22 @@ export default function DisplayActivityMap({ locations, kilometresSplit }: Displ
   }, [locations]);
 
   return (
-    <MapView style={[{ flex: 1 }]}>
-      <Camera
-        ref={cameraRef}
-        animationMode="flyTo"
-        animationDuration={1000}
-        bounds={{ ne: [minLng, minLat], sw: [maxLng, maxLat] }}
-        padding={{ paddingLeft: 20, paddingRight: 20, paddingBottom: 20, paddingTop: 20 }}
-      />
-      {locations.length > 1 && <RouteLine locations={locations} />}
-      <MapKmSplit kilometresSplit={kilometresSplit} />
-    </MapView>
+    <>
+      {Platform.OS === 'web' ? (
+        <MapboxWeb />
+      ) : (
+        <MapView style={[{ flex: 1 }]}>
+          <Camera
+            ref={cameraRef}
+            animationMode="flyTo"
+            animationDuration={1000}
+            bounds={{ ne: [minLng, minLat], sw: [maxLng, maxLat] }}
+            padding={{ paddingLeft: 20, paddingRight: 20, paddingBottom: 20, paddingTop: 20 }}
+          />
+          {locations.length > 1 && <RouteLine locations={locations} />}
+          <MapKmSplit kilometresSplit={kilometresSplit} />
+        </MapView>
+      )}
+    </>
   );
 }
