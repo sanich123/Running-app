d0dd5bd18bc3db622ea84633e3e57722e73e04fe
Alexander <aovoronin.piano@gmail.com>
2024-03-05 22:17:25 +0300

    fix: fix a couple of files

--- components/followers-page/followers-page.tsx
+++ components/followers-page/followers-page.tsx
@@ -1,5 +1,5 @@
 import ErrorComponent from '@C/error-component/error-component';
-import UserListItem from '@C/user-list-item/user-list-item';
+import UserListItemSimple from '@C/user-list-item-simple/user-list-item-simple';
 import { useGetFollowersByUserIdQuery } from '@R/runich-api/runich-api';
 import useRefresh from '@U/hooks/use-refresh';
 import { useAuth } from 'auth/context/auth-context';
@@ -11,7 +11,6 @@ export default function ListOfFollowers() {
   const { user } = useAuth();
   const { isLoading, error, isError, data: users, refetch } = useGetFollowersByUserIdQuery(`${user?.id}`);
   const { refreshing, onRefresh } = useRefresh(refetch);
-  console.log('followers', users);
 
   return (
     <SafeAreaView
@@ -23,7 +22,15 @@ export default function ListOfFollowers() {
             onRefresh={onRefresh}
             refreshing={refreshing}
             data={users}
-            renderItem={({ item }) => <UserListItem userId={item.user_id} />}
+            renderItem={({ item: { profile } }) => (
+              <UserListItemSimple
+                city={profile?.city}
+                name={profile?.name}
+                surname={profile?.surname}
+                profilePhoto={profile?.profilePhoto}
+                user_id={profile?.user_id}
+              />
+            )}
             ListEmptyComponent={
               <View style={{ alignItems: 'center' }}>
                 <Text variant="headlineLarge">There are no followers</Text>
