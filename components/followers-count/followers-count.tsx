2a54c2b85e493f04496dec9ca9d5876eee8c2e5d
Alexander <aovoronin.piano@gmail.com>
2024-03-11 18:05:23 +0300

    fix: some fix

--- components/followers-count/followers-count.tsx
+++ components/followers-count/followers-count.tsx
@@ -21,6 +21,7 @@ export default function FollowersCount() {
     error,
     data: followers,
   } = useGetFollowersByUserIdQuery(friendId ? `${friendId}` : `${user?.id}`);
+
   return (
     <Pressable
       onPress={() =>
