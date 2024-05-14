87e8e6fc87554e6f47a2a3350bb16447f328a509
Alexander <aovoronin.piano@gmail.com>
2024-05-14 23:45:05 +0300

    feat: remove nativewind 4 from project

--- components/metrics-item/metrics-item.tsx
+++ components/metrics-item/metrics-item.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Text, useTheme } from 'react-native-paper';
 
 type MetricsItemProps = {
@@ -13,7 +13,13 @@ export default function MetricsItem({ isMapVisible, title, metric, isCentral }: 
   const { dark } = useTheme();
   return (
     <View
-      className={`flex items-center justify-center ${isCentralAndIsNotMapVisible ? 'w-full' : 'w-1/2'} ${isMapVisible ? 'w-1/3 h-full' : 'h-1/3'}`}>
+      style={[
+        styles.layout,
+        {
+          width: isCentralAndIsNotMapVisible ? '100%' : isMapVisible ? '33.3%' : '50%',
+          height: isMapVisible ? '100%' : '30%',
+        },
+      ]}>
       <Text
         variant={`${isCentralAndIsNotMapVisible ? 'displayMedium' : 'headlineSmall'}`}
         style={{ color: dark ? 'white' : 'black' }}>
@@ -23,3 +29,11 @@ export default function MetricsItem({ isMapVisible, title, metric, isCentral }: 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  layout: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
