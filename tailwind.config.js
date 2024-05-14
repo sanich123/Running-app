bf56d692831af5c65f95804943078b8ccceeaee4
Alexander <aovoronin.piano@gmail.com>
2024-05-15 00:07:45 +0300

    feat: removing nativewind (#57)

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./app/**/*.tsx', './components/**/*.tsx'],
-  presets: [require('nativewind/preset')],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
