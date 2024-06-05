6616167fef897e03a423676482198b9e242056c8
Alexander <aovoronin.piano@gmail.com>
2024-06-05 23:10:58 +0300

    feat: improve user-search

--- components/users-page/users-page.tsx
+++ components/users-page/users-page.tsx
@@ -4,11 +4,15 @@ import { useGetFilteredUsersBySearchTextQuery } from '@R/runich-api/runich-api';
 import useDebounce from '@U/hooks/use-debounce';
 import useRefresh from '@U/hooks/use-refresh';
 import { useState } from 'react';
-import { View, FlatList } from 'react-native';
-import { ActivityIndicator, Divider, Text, TextInput } from 'react-native-paper';
+import { FlatList } from 'react-native';
+import { ActivityIndicator, Divider } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import EmptyUsersList from './empty-list/empty-list';
+import SearchInput from './search-input/search-input';
+
 export default function ListOfUsers() {
+  const [isInitial, setIsInitial] = useState(true);
   const [search, setSearch] = useState('');
   const debouncedSearch = useDebounce(search, 1000);
 
@@ -22,42 +26,28 @@ export default function ListOfUsers() {
   const { refreshing, onRefresh } = useRefresh(refetch);
 
   return (
-    <SafeAreaView edges={['bottom', 'left', 'right']} style={[{ flex: 1 }]}>
-      <View>
-        <TextInput
-          mode="outlined"
-          label="Введите имя или email юзера"
-          left={<TextInput.Icon icon="text-search" />}
-          style={{ marginLeft: 10, marginRight: 10 }}
-          onChangeText={(search) => setSearch(search)}
-          value={search}
-        />
-
-        <FlatList
-          onRefresh={onRefresh}
-          refreshing={refreshing}
-          data={users?.items}
-          renderItem={({ item }) => (
-            <UserListItem
-              city={item?.city}
-              name={item?.name}
-              surname={item?.surname}
-              profilePhoto={item?.profilePhoto}
-              placeholder={item?.profilePhotoBlurhash}
-              user_id={item?.user_id}
-            />
-          )}
-          ListEmptyComponent={
-            <View style={{ alignItems: 'center' }}>
-              <Text variant="headlineLarge">There are no users</Text>
-            </View>
-          }
-          ItemSeparatorComponent={() => <Divider />}
-          initialNumToRender={15}
-        />
-        {isLoading && <ActivityIndicator size="large" />}
-        {isError ? <ErrorComponent error={error} /> : null}
-      </View>
+    <SafeAreaView edges={['bottom', 'left', 'right']}>
+      <SearchInput setIsInitial={setIsInitial} setSearch={setSearch} search={search} />
+      <FlatList
+        onRefresh={onRefresh}
+        refreshing={refreshing}
+        data={users?.items}
+        renderItem={({ item }) => (
+          <UserListItem
+            city={item?.city}
+            name={item?.name}
+            surname={item?.surname}
+            profilePhoto={item?.profilePhoto}
+            placeholder={item?.profilePhotoBlurhash}
+            user_id={item?.user_id}
+          />
+        )}
+        ListEmptyComponent={<EmptyUsersList isInitial={isInitial} />}
+        ItemSeparatorComponent={() => <Divider />}
+        initialNumToRender={15}
+      />
+      {isLoading && <ActivityIndicator size="large" />}
+      {isError ? <ErrorComponent error={error} refetch={refetch} /> : null}
     </SafeAreaView>
   );
 }
