3e3269ddaae57a84eacdb5c563a4ca937e1f2968
Alexander <aovoronin.piano@gmail.com>
2024-06-14 18:21:58 +0300

    feat: change logic on native platforms, likes are rendering as bottom sheet

--- components/home-page/home-page.tsx
+++ components/home-page/home-page.tsx
@@ -1,8 +1,10 @@
 import { useAuth } from '@A/context/auth-context';
+import { ProfileType } from '@C/card/const ';
 import ErrorComponent from '@C/error-component/error-component';
 import FloatingBtn from '@C/floating-btn/floating-btn';
 import InfiniteScrollList from '@C/infinite-scroll-list/infinite-scroll-list';
 import UnsendedActivitiesIndicator from '@C/unsended-activities/unsended-activities-indicator';
+import UserListItem from '@C/user-list-item/user-list-item';
 import {
   setIsManualAdding,
   resetFinishedActivity,
@@ -11,10 +13,16 @@ import {
   setIsEditingActivity,
 } from '@R/activity/activity';
 import { setIsNeedToRefreshActivities } from '@R/main-feed/main-feed';
-import { runichApi, useGetActivitiesByUserIdWithFriendsActivitiesQuery } from '@R/runich-api/runich-api';
+import {
+  runichApi,
+  useGetActivitiesByUserIdWithFriendsActivitiesQuery,
+  useGetLikesByActivityIdQuery,
+} from '@R/runich-api/runich-api';
 import { useAppDispatch, useAppSelector } from '@R/typed-hooks';
+import { ModalLikesListContext } from '@U/context/activity-card-btns';
+import { BottomSheetFlatList, BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { useFocusEffect, useRouter } from 'expo-router';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { ActivityIndicator } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -29,7 +37,7 @@ export default function Feed() {
     { id: `${user?.id}`, page, take: 10 },
     { skip: !user?.id },
   );
-
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
   useFocusEffect(() => {
     if (needToRefreshActivities) {
       setPage(0);
@@ -37,33 +45,59 @@ export default function Feed() {
       dispatch(setIsNeedToRefreshActivities(false));
     }
   });
+  const { activityIdWhichLikesToDownload } = useAppSelector(({ mainFeed }) => mainFeed);
+  const { data: likes } = useGetLikesByActivityIdQuery(`${activityIdWhichLikesToDownload}`);
 
   return (
     <SafeAreaView edges={['left', 'right']} style={{ flex: 1, justifyContent: 'center' }}>
-      {isHaveUnsyncedActivity ? <UnsendedActivitiesIndicator /> : null}
-      {isLoading ? <ActivityIndicator size="large" testID="homeActivityIndicator" /> : null}
-      {error || data?.message ? <ErrorComponent error={error || data} refetch={refetch} /> : null}
-      {!data?.message && data?.activities?.length ? (
-        <>
-          <InfiniteScrollList
-            dataToRender={data?.activities}
-            page={page}
-            setPage={setPage}
-            refetch={refetch}
-            isLastPage={data?.isLastPage}
-          />
-          <FloatingBtn
-            onPressFn={() => {
-              dispatch(setIsManualAdding(true));
-              dispatch(setIsEditingActivity(false));
-              dispatch(resetFinishedActivity());
-              dispatch(resetManualData());
-              dispatch(resetActivityInfo());
-              push('/(tabs)/home/manual-activity');
-            }}
-          />
-        </>
-      ) : null}
+      <ModalLikesListContext.Provider value={{ modalRef: bottomSheetModalRef }}>
+        <BottomSheetModalProvider>
+          <>
+            <BottomSheetModal
+              ref={bottomSheetModalRef}
+              index={likes?.length < 3 ? 0 : 1}
+              snapPoints={['25%', '50%', '75%']}>
+              <BottomSheetFlatList
+                data={likes}
+                renderItem={({ item: { profile } }: { item: { profile: ProfileType } }) => (
+                  <UserListItem
+                    name={profile?.name}
+                    surname={profile?.surname}
+                    profilePhoto={profile?.profilePhoto}
+                    placeholder={profile?.profilePhotoBlurhash}
+                    city={profile?.city}
+                    user_id={profile?.user_id}
+                  />
+                )}
+              />
+            </BottomSheetModal>
+            {isHaveUnsyncedActivity ? <UnsendedActivitiesIndicator /> : null}
+            {isLoading ? <ActivityIndicator size="large" testID="homeActivityIndicator" /> : null}
+            {error || data?.message ? <ErrorComponent error={error || data} refetch={refetch} /> : null}
+            {!data?.message && data?.activities?.length ? (
+              <>
+                <InfiniteScrollList
+                  dataToRender={data?.activities}
+                  page={page}
+                  setPage={setPage}
+                  refetch={refetch}
+                  isLastPage={data?.isLastPage}
+                />
+                <FloatingBtn
+                  onPressFn={() => {
+                    dispatch(setIsManualAdding(true));
+                    dispatch(setIsEditingActivity(false));
+                    dispatch(resetFinishedActivity());
+                    dispatch(resetManualData());
+                    dispatch(resetActivityInfo());
+                    push('/(tabs)/home/manual-activity');
+                  }}
+                />
+              </>
+            ) : null}
+          </>
+        </BottomSheetModalProvider>
+      </ModalLikesListContext.Provider>
     </SafeAreaView>
   );
 }
--- utils/context/activity-card-btns.ts
+++ utils/context/activity-card-btns.ts
@@ -1,8 +1,13 @@
-import { createContext } from 'react';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { createContext, RefObject } from 'react';
 
 export const ActivityCardBtnsContext = createContext({
   isLoading: false,
   setIsLoading: (arg: boolean) => {},
   isDisabled: false,
   setIsDisabled: (arg: boolean) => {},
+});
+
+export const ModalLikesListContext = createContext({
+  modalRef: {} as RefObject<BottomSheetModal>,
 });
