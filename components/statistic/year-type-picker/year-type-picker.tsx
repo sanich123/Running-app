3f3b2cd705b277364ca6f98f00023755677784ce
Alexander <aovoronin.piano@gmail.com>
2024-09-06 15:00:41 +0300

    feat: add safe switching between years, when switched year doesnt have type in previous year

--- components/statistic/year-type-picker/year-type-picker.tsx
+++ components/statistic/year-type-picker/year-type-picker.tsx
@@ -2,6 +2,7 @@ import { useAuth } from '@A/context/auth-context';
 import { useGetYearsAndTypesQuery } from '@R/runich-api/runich-api';
 import { Picker } from '@react-native-picker/picker';
 import { YearsAndTypes, YearsAndTypesPickerProps } from './types';
+import { getTypesByYear } from './util';
 
 export default function YearTypePicker({
   setSelectedYear,
@@ -15,28 +16,45 @@ export default function YearTypePicker({
     data: availableYearsAndTypes,
     isError,
     isLoading,
+    isSuccess,
   } = useGetYearsAndTypesQuery(`${user?.id}`, { skip: !user?.id });
-
-  const types = availableYearsAndTypes?.find(({ year }: YearsAndTypes) => +year === selectedYear).types;
 
   return (
     <>
-      <Picker prompt="Выберите год" selectedValue={selectedYear} onValueChange={(year) => setSelectedYear(year)}>
-        {availableYearsAndTypes
-          ?.slice()
-          .sort((a: YearsAndTypes, b: YearsAndTypes) => +b.year - +a.year)
-          .map(({ year }: YearsAndTypes) => (
-            <Picker.Item label={year} value={year} enabled={!isError || !isLoading} key={year} />
-          ))}
-      </Picker>
-      <Picker
-        selectedValue={selectedType}
-        onValueChange={(type: string) => setSelectedType(type)}
-        prompt="Выберите тип активности">
-        {types?.map((type: string) => (
-          <Picker.Item label={type} value={type} enabled={!isError || !isLoading} key={type} />
-        ))}
-      </Picker>
+      {isSuccess && (
+        <>
+          <Picker
+            prompt="Выберите год"
+            selectedValue={selectedYear}
+            onValueChange={(year) => {
+              const typesInSelectedYear = getTypesByYear({
+                yearsAndTypes: availableYearsAndTypes,
+                selectedYear: year,
+              });
+              if (!typesInSelectedYear?.includes(selectedType)) {
+                setSelectedType(`${typesInSelectedYear?.[0]}`);
+              }
+              setSelectedYear(year);
+            }}>
+            {availableYearsAndTypes
+              ?.slice()
+              .sort((a: YearsAndTypes, b: YearsAndTypes) => +b.year - +a.year)
+              .map(({ year }: YearsAndTypes) => (
+                <Picker.Item label={year} value={year} enabled={!isError || !isLoading} key={year} />
+              ))}
+          </Picker>
+          <Picker
+            selectedValue={selectedType}
+            onValueChange={(type: string) => setSelectedType(type)}
+            prompt="Выберите тип активности">
+            {availableYearsAndTypes
+              ?.find(({ year }: YearsAndTypes) => +year === selectedYear)
+              ?.types?.map((type: string) => (
+                <Picker.Item label={type} value={type} enabled={!isError || !isLoading} key={type} />
+              ))}
+          </Picker>
+        </>
+      )}
     </>
   );
 }
